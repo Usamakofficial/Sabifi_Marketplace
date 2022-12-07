767c24f79e7741ceffb6b673eadadb76d67b9dcc
Usamakofficial <usamakofficial07@gmail.com>
2022-12-07 14:56:18 +0500

    add new large assets

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import timelessLogo from "../assets/sabifi.png";
+import { NavLink } from "react-router-dom";
+import Whitepaper from "./Whitepaper";
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4 mt-32">
+      <div className="flex flex-[0.25] justify-center items-center">
+        <img src={timelessLogo} alt="logo" className="w-32" />
+      </div>
+
+      <div className="flex flex-1 w-full justify-evenly items-center flex-wrap sm:mt-0 mt-5">
+      <ul
+        className="md:flex-[0.5] text-white md:flex
+        hidden"
+      >
+        <NavLink exact activeClassName='active_class' to='/'className="mr-8 cursor-pointer font-bold
+          ">Marketplace</NavLink>
+        <NavLink exact activeClassName='active_class' to='/token'className="mx-8 cursor-pointer font-bold
+          ">SABIFI Token</NavLink>
+        <NavLink exact activeClassName='active_class' to='/about'className="mx-8 cursor-pointer  font-bold
+          ">About Us</NavLink>
+           <div className="ml-16 cursor-pointer font-bold">
+          <Whitepaper/>
+        </div>
+      </ul>
+      </div>
+    </div>
+    <div className="flex flex-[0.25] py-8 justify-center items-center">
+      <p className="text-white text-right text-xs">
+        &copy;2022 All rights reserved
+      </p>
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,90 @@
+import timelessLogo from "../assets/sabifi.png";
+import { connectWallet } from "../Blockchain.Services";
+import { useGlobalState, truncate } from "../store";
+import { NavLink } from "react-router-dom";
+import Whitepaper from "./Whitepaper";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+const Header = () => {
+  const [connectedAccount] = useGlobalState("connectedAccount");
+  const [showIcons, setShowIcons] = useState(false);
+  return (
+    <nav className="w-4/5 flex  justify-between items-center py-6 mx-auto">
+      <div className="md:flex-[0.5] flex-initial">
+        <img
+          className="w-40 cursor-pointer"
+          src={timelessLogo}
+          alt="Timeless Logo"
+        />
+      </div>
+      <div className={showIcons ? "menu-link  mobile-menu-link" : "menu-link"}>
+      <ul
+        className="md:flex-[0.5] text-white md:flex
+        hidden list-none flex-row justify-between pr-6
+        text-lg
+        items-center flex-initial"
+      >
+        <NavLink
+          exact
+          activeClassName="active_class"
+          to="/"
+          className="mx-4 cursor-pointer  font-bold
+          "
+        >
+          Marketplace
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active_class"
+          to="/token"
+          className="mx-4 cursor-pointer font-bold
+          "
+        >
+          SABIFI Token
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active_class"
+          to="/about"
+          className="mx-4 cursor-pointer  font-bold
+          "
+        >
+          About Us
+        </NavLink>
+        <div className="mx-4 cursor-pointer font-bold">
+          <Whitepaper />
+        </div>
+      </ul>
+      </div>
+      <div className="hamburger_icon text-white md:hidden px-6 ml-28 pl-16">
+        <a href="#" onClick={()=>setShowIcons(!showIcons)}>
+      <MenuIcon/>
+      </a>
+      </div>
+
+     
+      {connectedAccount ? (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-l p-3
+          rounded-full cursor-pointer font-bold"
+        >
+          {truncate(connectedAccount, 4, 4, 11)}
+        </button>
+      ) : (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-xs p-3
+        font-bold
+          rounded-full cursor-pointer"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
